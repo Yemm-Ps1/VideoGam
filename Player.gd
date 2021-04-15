60cc80bc47c7c1c739d2ecb9d9868ae81954313c
Alexander Yemm <alexyemm@gmail.com>
2021-04-15 12:56:47 +0100

    Added screen dragging.

--- Player.gd
+++ Player.gd
@@ -3,65 +3,36 @@ extends Area2D
 var tree = [Vector2.ZERO]
 var tile_size = 64
 
-
-
-
-
-
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
 
-
-
-var speed = 2
 var update = true
 var velocity
 var direction
-
-var dir_enum =  {
-	"N" : Vector2.UP,
-	"S" : Vector2.DOWN,
-	"E" : Vector2.RIGHT,
-	"W" : Vector2.LEFT 
-	
-}
-
-func _translatedPos(pos):
-	var translatedPos = (pos / tile_size) - Vector2(0.5, 0.5)
-	return translatedPos
-	
-
-func _rotateTurn(dir):
-	print(dir)
-	match dir:
-		Vector2.UP:
-			rotation = 90
-		Vector2.DOWN:
-			rotation = 180
-		Vector2.LEFT:
-			rotation = 0
-		Vector2.RIGHT:
-			rotation = 270
-		_:
-			pass
-		
-
-
-
-
 var i = 0
+export var speed = 2
 
 func _process(delta):
 	var player_pos = _translatedPos(position)
 	if _movedTo(player_pos, tree[i]):
 		i+= 1
 		direction = player_pos.direction_to(tree[i])
+		velocity = direction*speed
 		_setDirection(direction)
 		return
+	elif i+1 == tree.size():
+		print("END")
+		return
 	
-	velocity = direction*speed
+	
 	position += velocity
+
+
+func _translatedPos(pos):
+	var translatedPos = (pos / tile_size) - Vector2(0.5, 0.5)
+	return translatedPos
+
 
 func _movedTo(current_pos, desired_pos):
 	if current_pos == desired_pos:
@@ -71,10 +42,8 @@ func _movedTo(current_pos, desired_pos):
 func _setDirection(dir):
 	match dir:
 		Vector2.UP:
-			print("UP")
 			rotation_degrees = 180
 		Vector2.DOWN:
-			print("DOWN")
 			rotation_degrees = 0
 		Vector2.LEFT:
 			rotation_degrees = 90
@@ -97,6 +66,14 @@ func _setDirection(dir):
 #		position += velocity
 #
 
+
+#var dir_enum =  {
+#	"N" : Vector2.UP,
+#	"S" : Vector2.DOWN,
+#	"E" : Vector2.RIGHT,
+#	"W" : Vector2.LEFT 
+#
+#}
 
 #func _process(delta):
 ##func _on_Maze_continue_signal():
